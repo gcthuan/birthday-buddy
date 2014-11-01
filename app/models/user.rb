325670e1c2e76374433dbd3b8fdfdbfe833ccbc2
altair <gcthuan@apcs.vn>
2014-11-01 22:33:12 +0700

    Add uniqueness to facebook_id and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  serialize :device_list
+  serialize :device_list, Array
+  validates :username, presence: true, uniqueness: true
+  validates :facebook_id, presence: true, uniqueness: true
+  validates :day, presence: true
+  validates :month, presence: true
+  validates :year, presence: true
 end
